4ecffa4a04d8aa484a3ac9c360b48872d0dd3835
Spencer Tank <sktank16@gmail.com>
2017-09-24 17:42:48 -0400

    leaflet map

--- spaces-app/src/map/map-view.tsx
+++ spaces-app/src/map/map-view.tsx
@@ -1,10 +1,32 @@
 import * as React from 'react'
+import * as L from 'leaflet'
 
 export class MapView extends React.Component<{}, {}> {
 
+
+  componentDidMount () {
+    const map = new L.Map('map', {
+        center: new L.LatLng(40.731253, -73.996139),
+        zoom: 12,
+      })
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1Ijoic2t0YW5rMTYiLCJhIjoiY2o3ejh0MG0yNjdwZDMycnJmaDlsam52NCJ9.r0aMy80G7VQsr2qVhEF4Qg'
+    }).addTo(map)
+
+    map.on('click', (e: L.LeafletMouseEvent) => {
+        let marker = L.marker(e.latlng)
+        .bindPopup('Popup')
+        .addTo(map)
+        .openPopup()
+      })
+  }
+
   render () {
-    return <div>
-      Map
+    return <div id='map'>
     </div>
   }
+
 }
