b2caa1ab0227fac7edfd9e3ccbd30a8da50a0013
Spencer Tank <sktank16@gmail.com>
2017-09-25 21:32:45 -0400

    move to using leaflet-react

--- spaces-app/src/map/map-view.tsx
+++ spaces-app/src/map/map-view.tsx
@@ -1,32 +1,20 @@
 import * as React from 'react'
 import * as L from 'leaflet'
+import { Map, TileLayer } from 'react-leaflet'
+
+const ACCESS_TOKEN = 'pk.eyJ1Ijoic2t0YW5rMTYiLCJhIjoiY2o3ejh0MG0yNjdwZDMycnJmaDlsam52NCJ9.r0aMy80G7VQsr2qVhEF4Qg'
 
 export class MapView extends React.Component<{}, {}> {
 
-
-  componentDidMount () {
-    const map = new L.Map('map', {
-        center: new L.LatLng(40.731253, -73.996139),
-        zoom: 12,
-      })
-
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1Ijoic2t0YW5rMTYiLCJhIjoiY2o3ejh0MG0yNjdwZDMycnJmaDlsam52NCJ9.r0aMy80G7VQsr2qVhEF4Qg'
-    }).addTo(map)
-
-    map.on('click', (e: L.LeafletMouseEvent) => {
-        let marker = L.marker(e.latlng)
-        .bindPopup('Popup')
-        .addTo(map)
-        .openPopup()
-      })
-  }
-
   render () {
-    return <div id='map'>
-    </div>
+    return <Map
+                center={new L.LatLng(40.731253, -73.996139)}
+                zoom={12}
+            >
+                <TileLayer
+                    url='http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png'
+                />
+            </Map>
   }
 
 }
