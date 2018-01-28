0de71f7020b8fbdde315b23b91b151173d1129e2
Spencer Tank <sktank@Spencers-MacBook-Pro.local>
2018-01-28 14:52:47 -0500

    expandable left sidebar for map view

--- spaces-app/src/home/home-view.tsx
+++ spaces-app/src/home/home-view.tsx
@@ -1,12 +1,100 @@
 import * as React from 'react'
+import { CSSProperties } from 'react';
+
 
 export class HomeView extends React.Component<{}, {}> {
 
   render () {
     return <div>
-      Home
+      <div className="sidebar-wrapper">
+      <div className="sidebar">
+          <div className="sidebar-header">
+            Spencer Tank
+          </div>
+        </div>
+      </div>
+
+      <div className="content-wrapper">
+        <HomeViewContent numBackgroundDivs={20} />
+      </div>
     </div>
   }
+}
+
+export interface IHomeViewProps
+{
+  numBackgroundDivs : number;
+}
+
+export interface IHomeViewState
+{
+  scrollPosition : number;
+  width          : number;
+}
+
+export class HomeViewContent extends React.Component<IHomeViewProps, IHomeViewState>
+{
+  container: any;
+  state : IHomeViewState
+  constructor(props: IHomeViewProps) {
+    super(props)
+    this.state = {
+      scrollPosition: 0,
+      width: 1000
+    }
+  }
+
+  setupWindowResizer = () => {
+    this.setState({width: this.container.offsetWidth})
+    window.addEventListener("optimizedResize", () => {
+        this.setState({width: this.container.offsetWidth})
+    });
+  }
+
+  componentDidMount() {
+    this.setupWindowResizer()
+  }
+
+  render() {
+    const { numBackgroundDivs } = this.props;
+    console.log(numBackgroundDivs);
+    return <div className="content" onScroll={this.handleScroll} ref={cont => this.container = cont}>
+      {Array.apply(null, {length: numBackgroundDivs}).map(Number.call, Number).map((num: number) =>
+      {
+        const width = this.state.width / numBackgroundDivs;
+        const sidebarWidth = 400;
+        const styleObject: CSSProperties = {
+          position : 'fixed',
+          top : '0px',
+          bottom: '0px',
+          left: sidebarWidth + num * width,
+          width: width,
+          transform: 'rotate(' + this.state.scrollPosition + 'deg)'
+
+        }
+        return (<div key={num} className={`background-div background-div-${num}`} style={styleObject}/>)
+      }
+      )}
+      <div className="content-scroll"/>
+    </div>
+  }
+
+  renderBackgroundDivs = () =>
+  {
+    const { numBackgroundDivs } = this.props;
+    for (let i = 0; i < numBackgroundDivs; i++) {
+
+    }
+   }
+
+  private handleScroll = () =>
+  {
+    this.setState({scrollPosition: this.state.scrollPosition + 1})
+    console.log(this.state.scrollPosition)
+  }
+  
+
+
 }
 
 
--- spaces-app/src/map/map-view.tsx
+++ spaces-app/src/map/map-view.tsx
@@ -4,17 +4,39 @@ import { Map, TileLayer } from 'react-leaflet'
 
 const ACCESS_TOKEN = 'pk.eyJ1Ijoic2t0YW5rMTYiLCJhIjoiY2o3ejh0MG0yNjdwZDMycnJmaDlsam52NCJ9.r0aMy80G7VQsr2qVhEF4Qg'
 
-export class MapView extends React.Component<{}, {}> {
+interface IState {
+    leftPanelExpanded: boolean;
+}
 
-  render () {
-    return <Map
-                center={new L.LatLng(40.731253, -73.996139)}
-                zoom={12}
-            >
-                <TileLayer
-                    url='http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png'
-                />
-            </Map>
+export class MapView extends React.Component<{}, IState> {
+    state: IState = { leftPanelExpanded: false }
+
+    private toggleLeftPanel = () => {
+        this.setState({ leftPanelExpanded: !this.state.leftPanelExpanded });
+    }
+
+    public render () {
+        const  { leftPanelExpanded } = this.state;
+        return (
+            <div className={`map-container ${leftPanelExpanded ? "left-expanded" : ""}`}>
+                <div className="map-panel-left">
+                    <div className="toggle-panel-button"
+                        onClick={this.toggleLeftPanel}    
+                    >
+                        { leftPanelExpanded ? "<<" : ">>" }
+                    </div>
+                </div>
+            <Map
+                    center={new L.LatLng(40.731253, -73.996139)}
+                    zoom={12}
+                >
+                    <TileLayer
+                        url='http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png'
+                    />
+                </Map>
+                <div className="map-panel-right" />
+            </div>
+        )
   }
 
 }
